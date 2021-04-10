fe49f9558120ccb69d5442468fe01dcc386ade15
achintya-saxena <74810469+achintya-saxena@users.noreply.github.com>
2021-04-10 14:56:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+var plane1,plane2;
+var plane1Img,plane2Img;
+var track,ground;
+var planes;
+
+var form, player, game;
+
+function preload() {
+plane1Img=loadImage("images/plane1.png");
+plane2Img=loadImage("images/plane2.png");
+
+track=loadImage("images/bg.png");
+
+
+}
+
+
+function setup(){
+  canvas = createCanvas(displayWidth-20,displayHeight-60);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+
+  
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState===2){
+    game.end();
+  }
+}
